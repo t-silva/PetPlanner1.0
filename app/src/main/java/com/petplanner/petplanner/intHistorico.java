eec19159cb8f16ae40aac69c8616538e57e31638
thiago <thiago@middle-earth.br>
2020-07-28 19:03:38 -0300

    Criando layout Historico

--- app/src/main/java/com/petplanner/petplanner/intHistorico.java
+++ app/src/main/java/com/petplanner/petplanner/intHistorico.java
@@ -1,23 +1,22 @@
 package com.petplanner.petplanner;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Color;
-import android.graphics.LinearGradient;
-import android.graphics.Paint;
-import android.graphics.Shader;
-import android.graphics.SweepGradient;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.CombinedChart;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -28,12 +27,12 @@ import com.github.mikephil.charting.data.CombinedData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import static java.lang.Float.MAX_VALUE;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class intHistorico extends AppCompatActivity {
     public static final String EXTRA_idPET = "idPet";
@@ -45,11 +44,58 @@ public class intHistorico extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
-
         super.onCreate(savedInstanceState);
+        final int[] idPet = {(Integer) getIntent().getExtras().get(EXTRA_idPET)};
         setContentView(R.layout.historico_activity);
+        LinearLayout lnChoose = findViewById(R.id.lnChoose);
+        carrega(idPet);
+        lnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final BottomSheetDialog bottomSheetChoose = new BottomSheetDialog(
+                        intHistorico.this,R.style.BottonSheetDialogTheme
+                );
+                final View bottomSheetPets = LayoutInflater.from(getApplicationContext())
+                        .inflate(
+                                R.layout.custom_bottom_list,
+                                (LinearLayout)findViewById(R.id.bottomSheetPets)
+                        );
+                petplannerDB = new PetplannerBD(getApplicationContext());
+                bd = petplannerDB.getReadableDatabase();
+                cursor =  bd.query(
+                        "PETS",
+                        new String[] {"_id","NOME","IMGRESID"},
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null);
+                ListView lvPets = (ListView) bottomSheetPets.findViewById(R.id.lvPets);
+                lvPets.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        idPet[0] = (int) id;
+                        bd = petplannerDB.getWritableDatabase();
+                        ContentValues cvAtual = new ContentValues();
+                        cvAtual.put("_idAtual", idPet[0]);
+                        bd.update("ATUAL",cvAtual,"_id = 1",null);
+                        carrega(idPet);
+                        //Toast.makeText(MainActivity.this, "Carregar id" + idPet[0],Toast.LENGTH_SHORT).show();
+                        bottomSheetChoose.dismiss();
+                    }
+                });
+                AdapterPetsChoose adapter = new AdapterPetsChoose(getApplicationContext(),cursor);
+                adapter.bindView(bottomSheetPets,getApplicationContext(),cursor);
+                bottomSheetChoose.setContentView(bottomSheetPets);
+                bottomSheetChoose.show();
+            }
+        });
 
-       int idPet = (Integer) getIntent().getExtras().get(EXTRA_idPET);
+
+
+    }
+    private void carrega(int idPet[]){
         try{
             petplannerDB = new PetplannerBD(getApplicationContext());
             bd = petplannerDB.getReadableDatabase();
@@ -58,13 +104,13 @@ public class intHistorico extends AppCompatActivity {
                     "PETS",
                     new String[] {"_id","NOME","RACA","IDADE", "SEXO","IMGRESID"},
                     "_id = ?",
-                    new String[]{Integer.toString(idPet)},
+                    new String[]{Integer.toString(idPet[0])},
                     null,
                     null,
                     null,
                     null);
             if (cursor.moveToFirst() ) {
-                de.hdodenhof.circleimageview.CircleImageView imgPerfil = findViewById(R.id.fotoCapa);
+                CircleImageView imgPerfil = findViewById(R.id.fotoCapa);
                 imgPerfil.setImageResource(cursor.getInt(5));
                 TextView txtNome = findViewById(R.id.txtNome);
                 txtNome.setText(cursor.getString(1));
@@ -74,26 +120,26 @@ public class intHistorico extends AppCompatActivity {
                 txtIdade.setText(cursor.getString(3));
                 TextView txtSexo = findViewById(R.id.txtSexo);
                 txtSexo.setText(cursor.getString(4));
+                Toast.makeText(this,cursor.getString(4),Toast.LENGTH_SHORT).show();
             }
             //RECUPERANDO MAIOR VALOR
             cursorMax = bd.query(
                     "ATIVIDADE",
                     new String[]{"_idPet","TEMPO"},
                     "_idPet = ? AND TEMPO=(SELECT MAX(TEMPO) FROM ATIVIDADE )",
-                    new String[]{Integer.toString(idPet)},
+                    new String[]{Integer.toString(idPet[0])},
                     null,
                     null,
                     null,
                     null
             );
-
             if(cursorMax.moveToFirst()) {
                 max = cursorMax.getFloat(1);
-                Toast.makeText(this,"Max: "+ max,Toast.LENGTH_LONG).show();
+//                Toast.makeText(this,"Max: "+ max,Toast.LENGTH_LONG).show();
             }
             else {
                 max =1;
-                Toast.makeText(this,"NAO ACHOU MAX",Toast.LENGTH_LONG).show();
+//                Toast.makeText(this,"NAO ACHOU MAX",Toast.LENGTH_LONG).show();
             }
             // RECUPERANDO MAIOR VALOR
 
@@ -103,9 +149,9 @@ public class intHistorico extends AppCompatActivity {
                     "HUMOR",
                     new String[]{"_id", "TIMESTAMP","STATUS"},
                     "_id = ?",
-                    new String[]{Integer.toString(idPet)},
+                    new String[]{Integer.toString(idPet[0])},
                     null,
-                    null,   
+                    null,
                     null,
                     null);
 
@@ -113,7 +159,7 @@ public class intHistorico extends AppCompatActivity {
                     "ATIVIDADE",
                     new String[]{"_idPet", "TIMESTAMP","TEMPO"},
                     "_idPet = ?",
-                    new String[]{Integer.toString(idPet)},
+                    new String[]{Integer.toString(idPet[0])},
                     null,
                     null,
                     null,
@@ -121,97 +167,115 @@ public class intHistorico extends AppCompatActivity {
 
 
 
-        /*      Iniciando Gráficos    */
+            /*      Iniciando Gráficos    */
 
-            mChart = (CombinedChart) findViewById(R.id.combinedChar);
-            mChart.getDescription().setText("Descrição Teste");
-            mChart.setDrawGridBackground(true); // Desenha o grid
-            mChart.setDrawBarShadow(true);
+            mChart = (CombinedChart) findViewById(R.id.cmbcHumorAtv);
+            mChart.getDescription().setText("");
+            mChart.setDrawGridBackground(false); // Desenha o grid
+            mChart.setDrawBarShadow(false);
+            mChart.setEnabled(false);
+            //mChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
+            //mChart.getAxisLeft().setDrawGridLines(false);
+            mChart.getXAxis().setDrawGridLines(false);
+            //mChart.getXAxis().setSpaceMax(0.1f);
             mChart.setHighlightFullBarEnabled(true);
+            mChart.setViewPortOffsets(7f,50f,50f,100f);
             mChart.setDrawOrder(new CombinedChart.DrawOrder[]{ //Definindo ordem (linha sobre Barras)
                     CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE
             });
 
-        /* Lidando com legendas */
+            /* Lidando com legendas */
 
             Legend l = mChart.getLegend();
             l.setWordWrapEnabled(true);
             l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-            l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+            l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
             l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
 
-        /*      Fim     */
+            /*      Fim     */
 
-        /*      Lidando com os Eixos     */
+            /*      Lidando com os Eixos     */
 
             YAxis rightAxis = mChart.getAxisRight();
             rightAxis.setDrawGridLines(false);
-            rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
-
+            rightAxis.setDrawAxisLine(false);
+            rightAxis.setAxisMinimum(1f); // this replaces setStartAtZero(true)
+            rightAxis.setGranularity(5f);
             YAxis leftAxis = mChart.getAxisLeft();
-            leftAxis.setDrawGridLines(false);
-            leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
-
-            XAxis xAxis = mChart.getXAxis();
-            xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
-            xAxis.setAxisMinimum(0f);
-            xAxis.setGranularity(1f);
-
-            /*      Lidando com labels inferiores   */
-
-            /*final ArrayList<String> mDays = new ArrayList<>();
+            leftAxis.setDrawGridLines(true);
+            leftAxis.setAxisMinimum(1f); // this replaces setStartAtZero(true)
 
 
 
-            xAxis.setValueFormatter(new IAxisValueFormatter() {
-                @Override
-                public String getFormattedValue(float value, AxisBase axis) {
-                    return mDays[(int) value % mDays.size()];
-                }
-            });*/
+            mChart.getAxisLeft().setAxisMinimum(0.1f);
+//            mChart.getAxisRight().setAxisMinimum(0.1f);
+            //           mChart.getXAxis().setAxisMinimum(0.1f);
+
+
 
             /*      FIM         */
 
-        /*      FIM     */
+            /*      FIM     */
 
             CombinedData data = new CombinedData();
             data.setData(generateLineData("Humor"));
             data.setData(generateBarData("Atividade Física"));
-            xAxis.setAxisMaximum(data.getXMax() + 0.25f);
+            XAxis xAxis = mChart.getXAxis();
+            YAxis yAxis = mChart.getAxisLeft();
+            yAxis.setAxisMaximum(4.4f);
+            yAxis.enableGridDashedLine(1f,4f,10f);
+            //yAxis.setDrawGridLines(true);
+            //yAxis.setDrawGridLinesBehindData(true);
 
+            yAxis.setGranularity(1f);
+            yAxis.setDrawLabels(false);
 
+            yAxis.setDrawAxisLine(false);
+            // yAxis.setSpaceMax(0f);
+            xAxis.setGranularity(1f);
+
+            /*      Lidando com labels inferiores   */
+            final ArrayList<String> mDatas;
+            mDatas = createLabels(cursorAtv);
+            xAxis.setValueFormatter(new IndexAxisValueFormatter(mDatas));
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+            xAxis.setAxisMaximum(data.getXMax() + 0.95f);
+            xAxis.setAxisMinimum(0f);
+            xAxis.setDrawAxisLine(false);
+            mChart.animateY(1000);
             mChart.setData(data);
-
             mChart.invalidate();
-
-    /*      Fim Iniciando Gráficos       */
-
-
-
-
+            /*      Fim Iniciando Gráficos       */
         } catch (SecurityException e){
             Toast.makeText(this, "Banco de dados Não Disponível",Toast.LENGTH_SHORT).show();
         }
     }
 
+
+    private ArrayList<String> createLabels(Cursor cursorLabel) {
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("");
+        if(cursorLabel.moveToFirst()) {
+            for (cursorLabel.moveToFirst(); !cursorLabel.isAfterLast(); cursorLabel.moveToNext()) {
+                String date_nf = cursorLabel.getString(1);
+                String[] output = date_nf.split("-");
+                String date_f = output[2] + "/" + output[1];
+                labels.add(date_f);
+            }
+        }
+        return labels;
+    }
     private BarData generateBarData(String label){
         ArrayList<BarEntry> entries  = new ArrayList<BarEntry>();
         entries = getBarEntries(entries);
         BarDataSet set1 = new BarDataSet(entries,label);
-        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setAxisDependency(YAxis.AxisDependency.RIGHT);
+        set1.setGradientColor(Color.rgb(73,99,255),Color.rgb(123,0,143));
+        set1.setDrawValues(false);
+
         BarData d = new BarData(set1);
-        d.setBarWidth(0.45f);
-        Paint mpaint = mChart.getRenderer().getPaintRender();
-            mpaint.setShader(
-                    new LinearGradient(0,0,0,600,
-                            Color.rgb(73,99,255),
-                            Color.rgb(123,0,143),
-                            Shader.TileMode.CLAMP)
-            );
-
+        d.setBarWidth(0.15f);
         return d;
-        //bardataset.setBarSpacePercent(80f);
-
         /*YAxis rightAxis = barChart.getAxisRight();
         YAxis leftAxis = barChart.getAxisLeft();
         XAxis xAxisBar = barChart.getXAxis();
@@ -231,23 +295,18 @@ public class intHistorico extends AppCompatActivity {
         //barChart.setBackgroundResource(R.drawable.activity_bcg);
        */
 
-
     }
-
     private ArrayList<BarEntry> getBarEntries(ArrayList<BarEntry> entries) {
         if (cursorAtv.moveToFirst()) {
-            float a = 0;
+            float a ;
             int b = 1;
             for (cursorAtv.moveToFirst(); !cursorAtv.isAfterLast(); cursorAtv.moveToNext()) {
                 a = cursorAtv.getFloat(2);
                 entries.add(new BarEntry(b++,a));
             }
-
         }
         return  entries;
-
     }
-
     private LineData generateLineData(String label){
         LineData d = new LineData();
         ArrayList<Entry> entries = new ArrayList<>();
@@ -257,60 +316,29 @@ public class intHistorico extends AppCompatActivity {
         set.setFillColor(Color.rgb(66, 192, 120)); //cor da linha
         set.setLineWidth(2.5f); //Tamanho da linha
         set.setDrawCircles(false); //remove circulos
+        set.setDrawValues(false);
         set.setMode(LineDataSet.Mode.CUBIC_BEZIER); //estilo da linha
-        set.setDrawValues(true);
         set.setAxisDependency(YAxis.AxisDependency.LEFT); //dependencia do eixo Y
         d.addDataSet(set);
         return d;
-
-
-        /*LineData data = new LineData(labels,linedataset);
-        lineChart.setData(data); // set the data and list of labels into chart
-
-
-        YAxis leftAxis = lineChart.getAxisLeft();
-        YAxis rightAxis = lineChart.getAxisRight();
-        XAxis xAxis = lineChart.getXAxis();
-
-        leftAxis.setEnabled(false);
-        rightAxis.setEnabled(false);
-        xAxis.setEnabled(false);
-
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setDrawGridLines(false);
-        xAxis.setDrawAxisLine(false);
-        lineChart.setDescription(null);  // set the description
-        lineChart.animateY(1000);
-*/
     }
-
     private ArrayList<Entry> getLineEntriesData(ArrayList<Entry> entries) {
         if (cursorH.moveToFirst()) {
-            float y = 0;
+            float y = 0f;
             int v = 1;
             for (cursorH.moveToFirst(); !cursorH.isAfterLast(); cursorH.moveToNext()) {
                 if (cursorH.getString(2).equals(getString(R.string.humorExc))) {
-                    y = max;
+                    y = 4f;
                 } else if (cursorH.getString(2).equals(getString(R.string.humorHappy))) {
-                    y = max * 3 / 4;
+                    y = 3f;
                 } else if (cursorH.getString(2).equals(getString(R.string.humorOk))) {
-                    y = max * 2 / 4;
+                    y = 2f;
                 } else if (cursorH.getString(2).equals(getString(R.string.humorBad))) {
-                    y = max / 4;
+                    y = 1f;
                 }
-                String date_nf = cursorH.getString(1);
-                String[] output = date_nf.split("-");
-                String date_f = output[1] + "/" + output[2];
                 entries.add(new Entry(v++,y));
             }
         }
-        /*entries.add(new Entry(1, 20));
-        entries.add(new Entry(2, 10));
-        entries.add(new Entry(3, 8));
-        entries.add(new Entry(4, 40));
-        entries.add(new Entry(5, 37));*/
-
-
         return entries;
     }
 }
